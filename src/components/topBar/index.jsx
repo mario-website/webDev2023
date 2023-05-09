660aa64d2497312ec8220ef8d4dd52539280bb9c
mario-website <mario.websie@gmail.com>
2023-05-09 16:05:03 +0200

    fixed not showing menu and add padding to topbar

--- src/components/topBar/index.jsx
+++ src/components/topBar/index.jsx
@@ -1,4 +1,4 @@
-import { useState, useEffect, useCallback } from 'react';
+import React, { useState, useEffect, useCallback } from 'react';
 import { FaBars } from 'react-icons/fa';
 import './index.css';
 
@@ -20,30 +20,50 @@ const Topbar = () => {
   }, []);
 
   const handleMenuClick = useCallback(() => {
-    setShowMenu(!showMenu);
-  }, [scrollPosition]);
-
+    //* This function uses a functional update with 'prev => !prev' to toggle 'showMenu', ensuring accurate updates by always using the latest state value.
+    //* This approach is recommended over using the possibly outdated 'showMenu' directly, which can be asynchronous in React.
+    setShowMenu((prev) => !prev);
+    //* As scrollPosition isn't used inside this function, it's no longer included in the useCallback dependencies.
+  }, []);
 
   return (
     <div className={`topbar ${scrollPosition > 0 ? 'scroll' : ''}`}>
       <div className='topbar-container'>
-        <div className="logo-container">
-        <svg width="26" height="19" viewBox="0 0 26 19" fill="none" xmlns="http://www.w3.org/2000/svg">
-          <path d="M20.605 8.05002C20.605 7.61502 20.75 7.18002 20.75 6.60002C20.75 3.41002 18.14 0.800018 14.95 0.800018C12.63 0.800018 10.745 2.10502 9.73001 3.99002C9.44001 3.84502 8.86001 3.70002 8.42501 3.70002C6.39501 3.70002 4.80001 5.29502 4.80001 7.32502C4.80001 7.61502 4.80001 7.90502 4.94501 8.05002C2.33501 8.48502 0.450012 10.515 0.450012 13.125C0.450012 15.88 2.77001 18.2 5.52501 18.2H20.025C22.78 18.2 25.1 15.88 25.1 13.125C25.1 10.515 23.215 8.34002 20.605 8.05002Z" fill="white"/>
-        </svg>
+        <div className='logo-container'>
+          <svg
+            width='26'
+            height='19'
+            viewBox='0 0 26 19'
+            fill='none'
+            xmlns='http://www.w3.org/2000/svg'>
+            <path
+              d='M20.605 8.05002C20.605 7.61502 20.75 7.18002 20.75 6.60002C20.75 3.41002 18.14 0.800018 14.95 0.800018C12.63 0.800018 10.745 2.10502 9.73001 3.99002C9.44001 3.84502 8.86001 3.70002 8.42501 3.70002C6.39501 3.70002 4.80001 5.29502 4.80001 7.32502C4.80001 7.61502 4.80001 7.90502 4.94501 8.05002C2.33501 8.48502 0.450012 10.515 0.450012 13.125C0.450012 15.88 2.77001 18.2 5.52501 18.2H20.025C22.78 18.2 25.1 15.88 25.1 13.125C25.1 10.515 23.215 8.34002 20.605 8.05002Z'
+              fill='white'
+            />
+          </svg>
 
-          <h1 className="name">CLOUDBUDGET</h1>
+          <h1 className='name'>CLOUDBUDGET</h1>
         </div>
 
         <div className={`menu ${showMenu ? 'show' : ''}`}>
-          <a className="link" href="#">OVERVIEW</a>
-          <a className="link" href="#">FEATURES</a>
-          <a className="link" href="#">TECHNOLOGY</a>
-          <a className="link" href="#">CONTACT</a>
-          <a className="link" href="#">SIGN UP</a>
+          <a className='link' href='#'>
+            OVERVIEW
+          </a>
+          <a className='link' href='#'>
+            FEATURES
+          </a>
+          <a className='link' href='#'>
+            TECHNOLOGY
+          </a>
+          <a className='link' href='#'>
+            CONTACT
+          </a>
+          <a className='link' href='#'>
+            SIGN UP
+          </a>
         </div>
 
-        <div className="burger-menu" onClick={handleMenuClick}>
+        <div className='burger-menu' onClick={handleMenuClick}>
           <FaBars />
         </div>
       </div>
