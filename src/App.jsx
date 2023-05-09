f63cc970ca3777a31ce64ab6c37da6e28e101b55
mario-website <mario.websie@gmail.com>
2023-05-09 18:23:39 +0200

    added containerPadding variable in css

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,25 @@
-import React from 'react'
-import './App.css'
-import TopBar from './components/topBar'
-import Intro from './sections/intro'
-import Features from './sections/features'
-import Technology from './sections/technology'
-import CTA from './sections/CTA'
-import Contact from './sections/contact'
+import React from 'react';
+import './App.css';
+import TopBar from './components/topBar';
+import Intro from './sections/intro';
+import Features from './sections/features';
+import Technology from './sections/technology';
+import CTA from './sections/CTA';
+import Contact from './sections/contact';
 
 function App() {
-
   return (
-    <div className="App">
-      <TopBar />
-      <Intro />
-      <Features />
-      <Technology />
-      <CTA />
-      <Contact />
+    <div className='App'>
+      <div className='container'>
+        <TopBar />
+        <Intro />
+        <Features />
+        <Technology />
+        <CTA />
+        <Contact />
+      </div>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
