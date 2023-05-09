b2b080a56627c592b4f9906229af59363b0c578b
mario-website <mario.websie@gmail.com>
2023-05-09 18:06:01 +0200

    add all elements to Contact section

--- src/sections/contact/index.jsx
+++ src/sections/contact/index.jsx
@@ -1,12 +1,79 @@
 import './index.css';
+import React, { useState } from 'react';
+import Text from '../../components/text';
+import Heading from '../../components/heading';
+import contactModel from '../../models/Contact.model';
 
 const Contact = () => {
+  const formDataDefultValues = {
+    firstName: '',
+    lastName: '',
+    phoneNumber: '',
+    serviceInterest: '',
+  };
+  const [formData, setFormData] = useState(formDataDefultValues);
+
+  const handleInputChange = (event) => {
+    setFormData((prev) => ({ ...prev, [event.target.name]: event.target.value }));
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log(`formData:`, formData);
+
+    // Clear the form
+    setFormData(formDataDefultValues);
+  };
 
   return (
-    <div className="contact">
-      :)
+    <div className='contact'>
+      <div className='container'>
+        <div className='description'>
+          <Heading level={2}>{contactModel.heading}</Heading>
+          <Text>{contactModel.text}</Text>
+        </div>
+        <div className='form'>
+          <form onSubmit={handleSubmit}>
+            <input
+              type='text'
+              name='firstName'
+              value={formData.firstName}
+              onChange={handleInputChange}
+              placeholder='First Name'
+              required
+            />
+            <input
+              type='text'
+              name='lastName'
+              value={formData.lastName}
+              onChange={handleInputChange}
+              placeholder='Last Name'
+              required
+            />
+            <input
+              type='tel'
+              name='phoneNumber'
+              value={formData.phoneNumber}
+              onChange={handleInputChange}
+              placeholder='Phone Number'
+              required
+            />
+            <input
+              type='text'
+              name='serviceInterest'
+              value={formData.serviceInterest}
+              onChange={handleInputChange}
+              placeholder='What service are you interested in?'
+              required
+            />
+            {/* <button type='submit' onClick={handleSubmit} className='btn'>
+            SUBMIT NOW
+          </button> */}
+          </form>
+        </div>
+      </div>
     </div>
-  )
+  );
 };
 
 export default Contact;
